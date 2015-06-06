3592dfdfdb604147c24ede203a6b2048c7668f6d
Jefferson Choi <jeffersonchoi@gmail.com>
2015-06-05 21:00:47 -0700

    working user and item model

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'users/:id/items/new' => 'users#user_item_new', as: :new_user_item
   post 'users/:id/items' => 'users#user_item_create'
   get 'users/:user_id/items/:id' => 'users#user_item_show', as: :user_item
-  get 'users/:user_id/items/:id/edit' => 'users#user_item_edit'
+  get 'users/:user_id/items/:id/edit' => 'users#user_item_edit', as: :edit_user_item
   patch 'users/:user_id/items/:id' => 'users#user_item_update'
   resources :users
 
