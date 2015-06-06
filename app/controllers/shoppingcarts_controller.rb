9e06b5008cccab93396563bb98d1d0452d87838d
Jefferson Choi <jeffersonchoi@gmail.com>
2015-06-06 09:56:59 -0700

    shoppingcart index added

--- app/controllers/shoppingcarts_controller.rb
+++ app/controllers/shoppingcarts_controller.rb
@@ -1,5 +1,11 @@
 class ShoppingcartsController < ApplicationController
 
+  def index
+    @shoppingcarts = Shoppingcart.all
+  end
 
+  # def show
+  #   @shoppingcart = Shoppingcart.find(params[:id])
+  # end
 
 end
