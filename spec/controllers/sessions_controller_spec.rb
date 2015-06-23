f4b1a206e6c3a3414c1407d0f8ca40651d9eb2ab
Jefferson Choi <jeffersonchoi@gmail.com>
2015-06-23 11:55:53 -0700

    Adds a passing test via rspec

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe SessionsController, type: :controller do
+
+
+    describe "session controller" do
+
+      it "should pass a test" do
+
+        assert true
+
+      end
+
+    end
+
+end
